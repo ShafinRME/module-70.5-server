e33a1e0134966f84ccc87810ed406f45d5163189
ShafinRME <shafin.rmedu4@gmail.com>
2022-05-09 19:36:16 +0600

    .env file is created and accessToken is also created

--- index.js
+++ index.js
@@ -11,48 +11,49 @@ app.use(cors());
 app.use(express.json());
 
 
-const verifyJWT = (req, res, next) =>{
+const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization;
-    if(!authHeader){
-        return res.status(401).send({message: 'unauthorized'});
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized' });
     }
     const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
-        if(err){
-            return res.status(403).send({message: 'forbidden'})
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden' })
         }
-        req.decoded= decoded;
+        req.decoded = decoded;
         next();
     })
 }
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('Hello from simple JWT Server')
 });
 
-app.post('/login', (req, res) =>{
+app.post('/login', (req, res) => {
     const user = req.body;
     console.log(user);
     // DANGER: Do not check password here for serious application
     // USE proper process for hashing and checking
     // After completing all authentication related verification, issue JWT token
-    if(user.email==='user@gmail.com' && user.password === '123456'){
+    if (user.email === 'user@gmail.com' && user.password === '123456') {
         const accessToken = jwt.sign({
-            email: user.email}, 
-            process.env.ACCESS_TOKEN_SECRET, 
-            {expiresIn: '1h'})
+            email: user.email
+        },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '1h' })
         res.send({
-            success: true,
-            accessToken: accessToken
+            success:
+                accessToken: accessToken
         })
     }
-    else{
-        res.status(401).send({success: false});
+    else {
+        res.status(401).send({ success: false });
     }
 })
 
-app.get('/orders', verifyJWT, (req, res) =>{
-    res.send([{id: 1, item: 'sunglass'}, {id: 2, item: 'moonglass'}])
+app.get('/orders', verifyJWT, (req, res) => {
+    res.send([{ id: 1, item: 'sunglass' }, { id: 2, item: 'moonglass' }])
 });
 
 app.listen(port, () => {
